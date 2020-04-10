51752105c5a1711ee2de77a9463449397656f5d7
raggedred <43875735+raggedred@users.noreply.github.com>
2020-04-10 12:10:38 +1000

    UnityApp

---
+++ Assets/Scenes/CarContoller2.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CarContoller2 : MonoBehaviour
+{
+
+	// PUBLIC
+	public Text Info;
+	public webSocketConnector driveController;
+	private controllerData2 myObject;
+
+
+	int i = 0;
+	bool sent = false;
+	void Start()
+	{
+		myObject = new controllerData2();
+	}
+	void Update()
+	{
+
+
+	}
+
+	public void sendAction(int action)
+	{
+		int value = 255;
+		bool received = false;
+		//myObject.speed = 0;
+		//myObject.direction = 0;
+		//myObject.action = action;
+		//string json = JsonUtility.ToJson(myObject);
+		driveController.setupSocket();
+		//driveController.WriteSocket(json);
+		do
+		{
+			driveController.WriteIntSocket(action);
+			Debug.Log("Sent: " + action);
+
+			string read = driveController.ReadSocket();
+			Debug.Log(read);
+
+			if (read == "" || read == "255" || read == null)
+			{
+				received = false;
+				Info.text = "Not Sent";
+			}
+			else
+			{
+
+				received = true;
+				value = int.Parse(read);
+				Info.text = "Sent";
+			}
+
+		} while (!received);
+
+
+
+		driveController.CloseSocket();
+
+	}
+
+	public void sendAction(string action)
+	{
+		int value = 255;
+		bool received = false;
+		int count = 0;
+		//myObject.speed = 0;
+		//myObject.direction = 0;
+		myObject.action = action;
+		string json = JsonUtility.ToJson(myObject);
+		driveController.setupSocket();
+		driveController.WriteSocket(json);
+		Debug.Log(json);
+		/*
+		do
+		{
+			char newChar = action.ToCharArray()[0];
+
+			driveController.WriteCharSocket(newChar);
+			Debug.Log("Sent: " + action);
+
+			string read = driveController.ReadSocket();
+			Debug.Log(read);
+			int result = IntParseFast(read);
+			if (result == 0 || result == 255 || result.Equals(null))
+			{
+				received = false;
+				Info.text = "Not Sent";
+			}
+			else
+			{
+
+				received = true;
+				value = int.Parse(read);
+				Info.text = "Sent";
+			}
+			count++;
+
+		} while (!received && count <= 10);
+
+	*/
+		string read = driveController.ReadSocket();
+		Debug.Log(read);
+		driveController.CloseSocket();
+
+	}
+
+	public static int IntParseFast(string value)
+	{
+		int result = 0;
+		for (int i = 0; i < value.Length; i++)
+		{
+			char letter = value[i];
+			result = 10 * result + (letter - 48);
+		}
+		return result;
+	}
+
+	[SerializeField]
+	private class controllerData2
+	{
+		public string action;
+	}
+}
+
---
+++ Assets/Scenes/webSocketConnector.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
+using System;
+
+public class webSocketConnector : MonoBehaviour
+{
+
+
+    internal Boolean socketReady = false;
+    TcpClient mySocket;
+    NetworkStream theStream;
+    StreamWriter theWriter;
+    StreamReader theReader;
+    public String Host = "192.168.1.223"; // eg String Host = "192.168.1.1";
+    public int Port = 5001; // eg Int32 Port = 5555;
+    public bool start;
+
+    String reader;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (start)
+        {
+            setupSocket();
+            //WriteSocket();
+            CloseSocket();
+            start = false;
+
+        }
+    }
+
+
+    public void setupSocket()
+    {                            // Socket setup here
+        try
+        {
+            //            Host = "192.168.2.17";
+            //            Port = 8000; 
+            mySocket = new TcpClient(Host, Port);
+            theStream = mySocket.GetStream();
+            theWriter = new StreamWriter(theStream);
+            theReader = new StreamReader(theStream);
+            socketReady = true;
+            //writeSocket("");
+            Debug.Log("Connected!");
+            socketReady = true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error:" + e);                // catch any exceptions
+        }
+    }
+
+    public void WriteSocket(string theLine)
+    {
+        if (!socketReady)
+            return;
+        String foo = theLine + "\r\n";
+        theWriter.Write(foo);
+        theWriter.Flush();
+    }
+    public void WriteIntSocket(int theLine)
+    {
+        if (!socketReady)
+            return;
+        theWriter.Write(theLine);
+        theWriter.Flush();
+    }
+    public void WriteCharSocket(char theLine)
+    {
+        if (!socketReady)
+            return;
+        theWriter.Write(theLine);
+        theWriter.Flush();
+    }
+
+    public string ReadSocket()
+    {
+        if (!socketReady)
+            return "";
+        try
+        {
+            return theReader.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error ReadSocket: " + e);
+            return "";
+        }
+    }
+
+    public void CloseSocket()
+    {
+        if (!socketReady)
+            return;
+        theWriter.Close();
+        theReader.Close();
+        mySocket.Close();
+        socketReady = false;
+    }
+}
